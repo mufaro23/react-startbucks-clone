8e0c38c4b7b82b6bbf1915048da7e39a93cf97c6
Mufaro Gomera <45699725+mufaro23@users.noreply.github.com>
2023-05-09 09:31:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {auth} from "./utils/firebase";
+import {login, logout, selectUser} from "./slices/userSlice";
+import HomePage from "./pages/HomePage";
+import AccountsPage from "./pages/AccountsPage";
+import MenuPage from "./pages/MenuPage";
+import FeaturedPage from "./pages/FeaturedPage";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // User is signed in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+          })
+        );
+      } else {
+        // User is signed out
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route exact path="/account/signin" element={<AccountsPage accountType="login" user={user} /> } />
+          <Route exact path="/account/create" element={<AccountsPage accountType="signup" user={user} /> } />
+          <Route exact path="/menu" element={<MenuPage user={user} /> } />
+          <Route exact path="/menu/featured" element={<FeaturedPage />} />
+        </Routes>
+        <div style={{ backgroundColor: "black", color: "white", width: "100%", padding: "16px", display: "flex", justifyContent: "space-between"}}>
+          <p></p>
+          <p style={{ marginRight: "auto", marginLeft: "auto"}}>Developed by Mufaro Gomera</p>
+        </div>
+      </Router>
     </div>
   );
 }
